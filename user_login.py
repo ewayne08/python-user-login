1106f2df3eea25a0b89dff3fff962605f482c02d
Ethan <ethanchristianwayne@gmail.com>
2021-09-27 12:47:41 -0400

    program complete

--- user_login.py
+++ user_login.py
@@ -2,10 +2,16 @@ Sys_User_Name = "EthanWayne"
 Sys_Password =  "123456"
 
 User_Name = input("Please enter your name: ")
-User_Password = input("Please enter your password")
+User_Password = input("Please enter your password: ")
 
 if User_Name != Sys_User_Name and User_Password == Sys_Password:
     print("Wrong user name...")
 
 elif User_Name == Sys_User_Name and User_Password != Sys_Password:
     print("Wrong password...")
+
+elif User_Name != Sys_User_Name and User_Password != Sys_Password:
+    print("Wrong user name and password!")
+
+else:
+    print("Login successfulS")
