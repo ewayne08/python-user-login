e6370c15f330ca92738b7237037c313ae0709afd
Ethan <ethanchristianwayne@gmail.com>
2021-09-27 12:44:35 -0400

    create scenarios

--- user_login.py
+++ user_login.py
@@ -1,2 +1,11 @@
 Sys_User_Name = "EthanWayne"
 Sys_Password =  "123456"
+
+User_Name = input("Please enter your name: ")
+User_Password = input("Please enter your password")
+
+if User_Name != Sys_User_Name and User_Password == Sys_Password:
+    print("Wrong user name...")
+
+elif User_Name == Sys_User_Name and User_Password != Sys_Password:
+    print("Wrong password...")
